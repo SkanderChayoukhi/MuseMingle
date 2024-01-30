39768dcea6c57fe592357f8f3ff84008280ef99a
arijzahi <ezzahiearije@gmail.com>
2024-01-31 00:48:25 +0100

    footer added to pagehome

--- pagehome.php
+++ pagehome.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
     <title>Document</title>
     <link rel="stylesheet" href="style1.css">
     
@@ -120,6 +122,7 @@
 
 
 
-<script src="app.js"></script>    
+<script src="app.js"></script> 
+<?php include "footer.php" ?>   
 </body>
 </html>
