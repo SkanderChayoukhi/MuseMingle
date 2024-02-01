10da0b5f778867393468a6fd894a9f68b063b25c
rana chouchen <ranachouchen4@gmail.com>
2024-02-01 19:19:27 +0100

    update footer

--- footer.php
+++ footer.php
@@ -1,46 +1,38 @@
 
-		
-		<footer style="background-color: #333; color: #fff; padding: 20px; text-align: center;">
-        
-        <div style="display: flex; justify-content: space-around;">
-            <div>
-                <h3>About Us</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-            <div>ghjk
-                <h3>Useful Links</h3>
-                <ul>
-                    <li><a href="#">Link 1</a></li>
-                    <li><a href="#">Link 2</a></li>
-                    <li><a href="#">Link 3</a></li>
-                </ul>
-            </div>
-            <div>
-                <h3>Location</h3>
-                <p><i class="fas fa-home"></i> Your Location</p>
-            </div>
-        </div>
+<footer style="background: white; padding: 20px; text-align: left; font-family: 'Arial', sans-serif;">
 
-        
-        <div style="margin-top: 20px;">
-            <h3>Get in Contact?</h3>
-            <div>
-                <a href="#"><i class="fab fa-github"></i></a>
-                <a href="#"><i class="fab fa-twitter"></i></a>
-                <a href="#"><i class="fab fa-instagram"></i></a>
-            </div>
-            <p>You want to get in touch? We are just a message away!</p>
-        </div>
-        <div style="margin-top: 20px;">
-            <nav>
-                <a href="#">Home</a> |
-                <a href="#">Services</a> |
-                <a href="#">Contact</a>
-            </nav>
-            <p>&copy; 2024 MuseMingle</p>
-        </div>
-    </footer>
+<div style="display: flex; justify-content: space-around; color: #808080;">
+    <div style="width: 30%;">
+        <h3 style="color: rgb(164, 7, 7)">About Us</h3>
+        <p style="color: black;">MuseMingle, our platform dedicated to the celebration of art, serves as a captivating showcase for talented artists. Whether it's the mesmerizing lens of photography or the strokes of a painter's brush, we are confident that you'll discover the missing piece with us. Feel free to explore our diverse collection and immerse yourself in the world of creativity.</p>
+    </div>
+    <div>
+        <h3 style="color: rgb(164, 7, 7)">Useful Links</h3>
+        <ul style="list-style: none; padding: 0; margin: 0;">
+            <li><a href="#" style="color: #808080; text-decoration: none; transition: color 0.3s ease;" onmouseover="this.style.color='rgb(164, 7, 7)'" onmouseout="this.style.color='#d3d3d3'">Home</a></li>
+            <li><a href="#" style="color: #808080; text-decoration: none; transition: color 0.3s ease;" onmouseover="this.style.color='rgb(164, 7, 7)'" onmouseout="this.style.color='#d3d3d3'">Gallery</a></li>
+            <li><a href="#" style="color: #808080; text-decoration: none; transition: color 0.3s ease;" onmouseover="this.style.color='rgb(164, 7, 7)'" onmouseout="this.style.color='#d3d3d3'">Artists</a></li>
+        </ul>
+    </div>
+    <div style="text-align: center;">
+        <h3 style="color: rgb(164, 7, 7)">Location</h3>
+         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3190.926992420169!2d10.185327175691967!3d36.89209477222108!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12e2cba9a188b601%3A0xee2a38ad70274da0!2sSup&#39;com%20Junior%20Entreprise!5e0!3m2!1sfr!2stn!4v1706652523085!5m2!1sfr!2stn" width="200" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+</div>
 
-		
-		
+<div style="margin-top: -35px; color: #d3d3d3; text-align: center;">
+    <h3 style="color: rgb(164, 7, 7)">Get in Contact?</h3>
+    <div class="social" style="display: flex; margin: 3px; justify-content: center;">
+        <a href="https://github.com/SkanderChayoukhi/MuseMingle" style="color: #d3d3d3; text-decoration: none; transition: color 0.3s ease; margin-right: 10px;" onmouseover="this.style.color='rgb(164, 7, 7)'" onmouseout="this.style.color='#808080'"><i class="fa fa-github" style="font-size: 20px;"></i></a>
+        <a href="https://github.com/SkanderChayoukhi/MuseMingle" style="color: #d3d3d3; text-decoration: none; transition: color 0.3s ease; margin-right: 10px;" onmouseover="this.style.color='rgb(164, 7, 7)'" onmouseout="this.style.color='#808080'"><i class="fa fa-twitter" style="font-size: 20px;"></i></a>
+        <a href="https://github.com/SkanderChayoukhi/MuseMingle" style="color: #d3d3d3; text-decoration: none; transition: color 0.3s ease; margin-right: 10px;" onmouseover="this.style.color='rgb(164, 7, 7)'" onmouseout="this.style.color='#808080'"><i class="fa fa-linkedin" style="font-size: 20px;"></i></a>
+    </div>
+    <p style="color: rgb(164, 7, 7);">You want to get in touch? We are just a message away!</p>
+</div>
+
+<div style="margin-top: 5px; color: #d3d3d3; text-align: center;">
+    
+    <div style="background: rgb(164, 7, 7); height: 30px; display: flex; align-items: center; justify-content: center;"><p>&copy; 2024 MuseMingle</p></div>
+</div>
+</footer>
 
